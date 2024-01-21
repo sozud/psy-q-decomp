eb0858522c17a28e40109fee555cd9c15949a8d4
sozud <122322823+sozud@users.noreply.github.com>
2024-01-21 15:59:13 -0800

    updates

--- split.sh
+++ split.sh
@@ -1,2 +1,2 @@
 export MYDIR=$(pwd)
-cd libsnd_linked && sh build.sh && cd $MYDIR && python3 ./gen.py && rm -rf asm && rm -rf src && python3 ./splat/split.py --verbose ./splat.yaml 
+cd libsnd_linked && sh build.sh && cd $MYDIR && mkdir -p config && python3 ./gen.py && rm -rf asm && rm -rf src && python3 ./splat/split.py --verbose ./splat.yaml 
