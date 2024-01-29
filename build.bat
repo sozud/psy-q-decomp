384db61fbfada82c1998005c21e046aeb6fb5017
sozud <122322823+sozud@users.noreply.github.com>
2024-01-28 18:42:25 -0800

    debug

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
-set COMPILER_PATH=G:/build
-set C_INCLUDE_PATH=G:/psy-q/3.5/PSX/include
-set PSYQ_PATH=G:/build
+set COMPILER_PATH=%CD%build
+set C_INCLUDE_PATH=%CD%psy-q/3.5/PSX/include
+set PSYQ_PATH=%CD%build
 set arg1=%1
 set arg2=%2
 shift
 shift
 dir build
-G:\build\ccpsx.exe -v -O2 -g0 -G0 -funsigned-char -c -I./src/snd %arg1% -o%arg2%
+%CD%build\ccpsx.exe -v -O2 -g0 -G0 -funsigned-char -c -I./src/snd %arg1% -o%arg2%
