1c957464898d72e2e2edf86833e5085e33e24f3d
sozud <122322823+sozud@users.noreply.github.com>
2024-02-10 16:45:48 -0800

    Fix extract command

--- extract.sh
+++ extract.sh
@@ -1,2 +1,2 @@
 rm -rf asm
-cargo run --release --manifest-path ./tools/psy-q-splitter/splitter/Cargo.toml ./psy-q/3.5/PSX/LIB/LIBSND.LIB ./asm
+cargo run --release --manifest-path ./tools/psy-q-splitter/splitter/Cargo.toml extract ./psy-q/3.5/PSX/LIB/LIBSND.LIB ./asm
