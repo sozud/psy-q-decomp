dbb2f7430a802c7419959629c8ddd227fb725795
sozud <122322823+sozud@users.noreply.github.com>
2024-03-01 00:38:09 -0800

    fix

--- build.bat
+++ build.bat
@@ -1,7 +1,4 @@
 @echo off
-set COMPILER_PATH=%CD%build
-set C_INCLUDE_PATH=%CD%include
-set PSYQ_PATH=%CD%build
 set arg1=%1
 set arg2=%2
 set arg3=%3
@@ -9,5 +6,9 @@ set arg4=%4
 shift
 shift
 shift
+shift
+set COMPILER_PATH=%CD%build\%arg4%
+set C_INCLUDE_PATH=%CD%include
+set PSYQ_PATH=%CD%build\%arg4%
 @echo on
 %CD%build\%arg4%\ccpsx.exe %arg3% -O2 -g0 -G0 -funsigned-char -c -I./src/snd %arg1% -o%arg2%
