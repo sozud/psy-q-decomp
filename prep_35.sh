9aea7b3387d6d6cf43e6d91f7ede478e372ae969
sozud <122322823+sozud@users.noreply.github.com>
2024-03-02 13:46:12 -0800

    fix prep

--- prep_35.sh
+++ prep_35.sh
@@ -2,7 +2,7 @@ mkdir -p build/3.5
 # put toolchain together
 cp -r ./psy-q/3.3/PSYQ/* build/3.5
 
-wget -O https://github.com/Xeeynamo/sotn-decomp/releases/download/cc1-psx-26/cc1-psx-26.tar.gz
+wget -O cc1-psx-26.tar.gz https://github.com/Xeeynamo/sotn-decomp/releases/download/cc1-psx-26/cc1-psx-26.tar.gz
 tar -xvf cc1-psx-26.tar.gz -C build/3.5
 cp -r ./psy-q/cc_2.60/* build/3.5
 # replace aspsx with newer one
